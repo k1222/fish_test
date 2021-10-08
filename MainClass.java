9bb1657efd11a828ff282c6b199679d0836c5bfc
k1222 <243911961@qq.com>
2021-10-08 15:20:51 +0800

    test4 (#5)
    * test1 (#7)
    
    test1
    
    * fish test2
    
    fish test2
    
    * Test123 k (#9)
    
    * test1
    
    test1
    
    * test2
    
    test2
    
    * add java file
    
    Co-authored-by: yuyang <fishyy1989@sina.com>

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+package org.devio.hi.library;
+
+/**
+ * @ProjectName: hi-library
+ * @Package: org.devio.hi.library
+ * @ClassName: MainClass
+ * @Description: java类作用描述
+ * @Author: Fish
+ * @CreateDate: 2021/10/8
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
